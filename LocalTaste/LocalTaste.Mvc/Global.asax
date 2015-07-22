223d1aa7a8046289950926a22fcf852ed5755dcd
Eduardo Lozano <EduardoL94@live.com>
2015-07-21 23:22:12 -0400

    Initial Commit

---
+++ LocalTaste/LocalTaste.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocalTaste.Mvc.MvcApplication" Language="C#" %>
---
+++ LocalTaste/LocalTaste.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocalTaste.Mvc.Startup))]
+namespace LocalTaste.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
